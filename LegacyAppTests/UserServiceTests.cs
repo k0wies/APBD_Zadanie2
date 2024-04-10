e49509949ab6afa7fa09f451753122ff872e49d7
s24904 <s24904@pjwstk.edu.pl>
2024-04-10 20:11:59 +0200

    Added some tests

--- LegacyAppTests/UserServiceTests.cs
+++ LegacyAppTests/UserServiceTests.cs
@@ -10,22 +10,40 @@ namespace LegacyAppTests
         {
             //Arrange
             var service = new UserService();
+
             //Act
             var result = service.AddUser(null, null, "kowalski@wp.pl", new DateTime(1980, 1, 1), 1);
+
             //Assert
             Assert.False(result);
 
         }
 
+        [Test]
+        public void AddUser_Should_Return_False_When_Email_In_Wrong_Form()
+        {
+            //Arrange
+            var service = new UserService();
 
+            //Act
+            var result = service.AddUser("Ferdynand", "Kiepski", "kiepskiwppl", new DateTime(1980, 1, 1), 1);
+
+            //Assert
+            Assert.False(result);
+
+        }
+
+        [Test]
         public void AddUser_Should_Throw_Exception_When_User_Does_Not_Exist()
         {
             //Arrange
             var service = new UserService();
-            //Act
-            var result = service.AddUser(null, null, "kowalski@wp.pl", new DateTime(1980, 1, 1), 1);
-            //Assert
-            Assert.False(result);
+
+            //Act and Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                _ = service.AddUser("Arnold", "Boczek", "kowalski@wp.pl", new DateTime(1980, 1, 1), 1);
+            });
 
         }
     }
